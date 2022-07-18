fde5531a3b3ad8c94ae3111097d41733de77d425
Valgaev Dmitriy <104302854+MuadDib3@users.noreply.github.com>
2022-07-18 21:15:06 +0300

    Add files via upload

---
+++ moxa/js/script.js
@@ -0,0 +1,21 @@
+$(window).scroll(function(){
+   if($(window).scrollTop() <= 96){
+      $(".content-header").css ("height", 96);
+      $(".content-header-after").css("margin-top", 27);
+      $(".content-header__flexbox").css("margin-top", 27);
+      $(".content-header-after").css("opacity", 0);
+   }
+   if ($(window).scrollTop() > 97) {
+
+      $(".content-header").height (70);
+       $(".content-header-after").css("margin-top", 14);
+       $(".content-header-after").css("opacity", 1);
+       $(".content-header__flexbox").css("margin-top", 14);
+   }
+});
+
+// const logoDecoration = document.getElementsByClassName('logo-decoration');
+// console.log(logoDecoration);
+// window.addEventListener('wheel', function () {
+//    logoDecoration.classList.add ("active");
+// )};
