e4a7d29251f2f14b1083199122369a712835dc9f
ValgaevD <myaddib3@gmail.com>
2022-07-27 20:57:01 +0300

    ok its fine now

--- moxa/js/script.js
+++ moxa/js/script.js
@@ -16,21 +16,68 @@ $(window).scroll(function(){
 
 
 
-function getScroll () {
-   const scroll = window.pageYOffset;
-   return scroll;
-}
-
-function clickOnLink () {
-   document.location.href = "/moxa/index.html";
-}
-
-
 function backToCurrentScroll () {
    document.location.href = "/moxa/index.html";
-   window.scrollTo(0, scroll);
+   window.scrollTo(0, 300);
 }
 
-function seeScroll () {
-   console.log(getScroll);
+//!---------------------scroll----------------------------------------//
+const about = document.querySelector(".about__text");
+function scrollToAbout () {
+   about.scrollIntoView({block: "center", behavior: "smooth"});
 }
+
+const portfolio = document.querySelector(".about__portfolio");
+function scrollToPortfolio () {
+   portfolio.scrollIntoView({block: "start", behavior: "smooth"});
+}
+
+const articles = document.querySelector(".articles");
+function scrollToArticles () {
+   articles.scrollIntoView({block: "start", behavior: "smooth"});
+}
+
+//!------------------------scroll-animation------------------------//
+
+const throttle = (func, limit) => {
+   let lastFunc;
+   let lastRan;
+   return function() {
+     const context = this;
+     const args = arguments;
+     if (!lastRan) {
+       func.apply(context, args);
+       lastRan = Date.now();
+     } else {
+       clearTimeout(lastFunc);
+       lastFunc = setTimeout(function() {
+         if ((Date.now() - lastRan) >= limit) {
+           func.apply(context, args);
+           lastRan = Date.now();
+         }
+       }, limit - (Date.now() - lastRan));
+     }
+   }
+ }
+
+window.addEventListener('wheel', throttle(function Animation() {
+   const logoDecotation = document.querySelectorAll('.logo-decoration__aimation');
+   for (let i = 0; i < logoDecotation.length; i++) {
+      logoDecotation[i].classList.add('logo-decoration_scroll');
+   }
+ function removeAnimation() {
+      for (let i = 0; i < logoDecotation.length; i++) {
+      logoDecotation[i].classList.remove('logo-decoration_scroll');
+   }}
+   setTimeout(removeAnimation, 6000);
+}, 7000));
+
+
+
+//  window.addEventListener('scroll', function removeAnimation() {
+//    const logoDecotation = document.querySelectorAll('.logo-decoration__aimation');
+//    for (let i = 0; i < logoDecotation.length; i++) {
+//       logoDecotation[i].classList.remove('logo-decoration_scroll');
+//    }
+//  });
+ 
