a14a3d2e75cb8d87c3df8fbdcaf57a163a6b0472
MuadDib3 <myaddib3@gmail.com>
2022-11-12 22:11:44 +0600

    after a long break

--- Jumbo/js/script.js
+++ Jumbo/js/script.js
@@ -1,7 +1,5 @@
 $(".use-cases-text__nav-item").on("click", function() {
 
-   $(".use-cases-text__nav-description").slideUp('fast');
-   $(".use-cases-text__nav-button").slideUp('fast');
    $(this).find('.use-cases-text__nav-description').slideToggle('fast');
    $(this).find('.use-cases-text__nav-button').slideToggle('fast');
 
